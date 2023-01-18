759d5d3c0e1fa46333bf662670bbbb22090e267a
ellie <ellie@tubbo.net>
2023-01-18 00:35:35 +0000

    yep

--- database/migrations/2023_01_17_151827_add_auto_mount_to_mounts_table.php
+++ database/migrations/2023_01_17_151827_add_auto_mount_to_mounts_table.php
@@ -4,30 +4,30 @@
  use Illuminate\Database\Schema\Blueprint;
  use Illuminate\Database\Migrations\Migration;
 
- class AddAutoMountToMountsTable extends Migration
- {
-     /**
-      * Run the mount migrations.
-      *
-      * @return void
-      */
-     public function up()
-     {
-         Schema::table('mounts', function (Blueprint $table) {
-             $table->boolean('mount_on_install')->after('user_mountable')->default(false);
-             $table->boolean('auto_mount')->after('mount_on_install')->default(false);
-         });
-     }
+class AddAutoMountToMountsTable extends Migration
+{
+    /**
+     * Run the mount migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mounts', function (Blueprint $table) {
+            $table->boolean('mount_on_install')->after('user_mountable')->default(false);
+            $table->boolean('auto_mount')->after('mount_on_install')->default(false);
+        });
+    }
 
-     /**
-      * Reverse the migrations.
-      *
-      * @return void
-      */
-     public function down()
-     {
-         Schema::table('mounts', function (Blueprint $table) {
-             // todo xd
-         });
-     }
- }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mounts', function (Blueprint $table) {
+            // todo xd
+        });
+    }
+}
