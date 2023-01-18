51eeec7ada8cf53f0591b9923f1bde2a5df039f9
ellie <ellie@tubbo.net>
2023-01-18 01:10:29 +0000

    help

--- config/app.php
+++ config/app.php
@@ -11,7 +11,7 @@ return [
     | change this value if you are not maintaining your own internal versions.
     */
 
-    'version' => '1.11.2.1',
+    'version' => 'helpme',
 
     /*
     |--------------------------------------------------------------------------
